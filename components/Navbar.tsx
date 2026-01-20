3f1baaa9233bd820871c9f44664b347da46ccde6
Charles Evangelista <kizaru03012002@gmail.com>
2026-01-20 20:06:10 +0800

    first commit

---
+++ components/Navbar.tsx
@@ -0,0 +1,26 @@
+import Link from 'next/link'
+import Image from 'next/image';
+const Navbar = () => {
+  return (
+    <header>
+    <nav>
+        <Link href="/" className='logo'>
+            <Image 
+            src="/icons/logo.png"
+            alt="logo"
+            width={24}
+            height={24}
+            />
+            <p>DevEvents</p>
+        </Link>
+        <ul>
+            <Link href="/Home">Home</Link>
+            <Link href="/Events">Events</Link>
+            <Link href="/CreateEvent">Create Event</Link>    
+        </ul>
+    </nav>
+    </header>
+  )
+}
+
+export default Navbar
---
+++ types/index.ts
@@ -0,0 +1,8 @@
+export default interface CardEvent {
+  title: string;
+  image: string;
+  slug: string;
+  location?: string;
+  date?: string;
+  time?: string;
+}
